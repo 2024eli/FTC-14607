358d617a3e7b0528b5146121763d6074118ebb34
Winston <Winstonhy.gan@gmail.com>
2022-12-10 10:02:44 -0500

    is this how u use github

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelepathicReSUScitation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelepathicReSUScitation.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -29,7 +31,7 @@ public class TelepathicReSUScitation extends LinearOpMode {
 
         waitForStart();
 
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             prevGamepad1.copy(currentGamepad1);
             currentGamepad1.copy(gamepad1);
             prevGamepad2.copy(currentGamepad2);
@@ -44,7 +46,7 @@ public class TelepathicReSUScitation extends LinearOpMode {
             float x = gamepad1.left_stick_x * speedFactor;
             float rx = gamepad1.right_stick_x * speedFactor;
 
-            float frontLeftPower = (y + x + rx) ;
+            float frontLeftPower = (y + x + rx);
             float backLeftPower = (y - x + rx) * 1.25f;
             float frontRightPower = (y - x - rx);
             float backRightPower = (y + x - rx) * 1.3f;
@@ -75,13 +77,31 @@ public class TelepathicReSUScitation extends LinearOpMode {
             if (gamepad2.right_bumper) control.setSwivel(control.swivel.getPosition() + 0.02);
             else if (gamepad2.left_bumper) control.setSwivel(control.swivel.getPosition() - 0.02);
 
-            // lift
-            if (gamepad2.right_trigger > 0) control.setLift(control.lift.getPosition() + 0.02);
-            else if (gamepad2.left_trigger > 0) control.setLift(control.lift.getPosition() - 0.02);
+            //lift
+            if (gamepad2.right_trigger > 0) control.setLift(control.lift.getPosition() + 0.05);
+            else if (gamepad2.left_trigger > 0) control.setLift(control.lift.getPosition() - 0.05);
 
+            //slides alex's
+//            control.rightSlide.setPower(gamepad2.left_stick_y);
+//            control.leftSlide.setPower(gamepad2.left_stick_y);
+            if (gamepad2.left_stick_y > 0) {
+                control.leftSlide.setTargetPosition(control.leftSlide.getCurrentPosition() - 500);
+                control.leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                control.leftSlide.setPower(-1);
+                control.rightSlide.setTargetPosition(control.rightSlide.getCurrentPosition() + 500);
+                control.rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                control.rightSlide.setPower(1);
+            } else if (gamepad2.left_stick_y < 0) {
+                control.leftSlide.setTargetPosition(control.leftSlide.getCurrentPosition() + 500);
+                control.leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                control.leftSlide.setPower(-1);
+                control.rightSlide.setTargetPosition(control.rightSlide.getCurrentPosition() - 500);
+                control.rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                control.rightSlide.setPower(1);
 
-            telemetry.addLine("Opmode Running");
-            telemetry.update();
+                telemetry.addLine("Opmode Running");
+                telemetry.update();
+            }
         }
     }
 }
